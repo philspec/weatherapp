8e76054dab292a74029b69fe07b7928c2b0a6381
phillips <qwertysonata@gmail.com>
2024-06-14 09:35:34 +0530

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,28 @@
 //a simple react component
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 import supabase from './supabaseclient.js';
 
 
 
 export default function Home() {
   const [error, setError] = useState("")
+  const navigate = useNavigate()
 
-   async function handleclick(){
-
-  const { error } = await supabase.auth.signOut()
-  setError(error)
-
-   }
+  const handleClick = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      setError(error.message);
+    } else {
+      console.log("Signed out successfully");
+      navigate('/login');
+    }
+  };
 
   return (
     <div className="grid w-screen h-screen place-items-center">
       <h1>Home</h1>
-      <button onClick={handleclick}>Sign out</button>
+      <button onClick={handleClick}>Sign out</button>
       {error && <p className="text-red-600">{error}</p>}
     </div>
   );
