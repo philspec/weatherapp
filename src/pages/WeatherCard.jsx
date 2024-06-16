b2583061929c0eb719bb5ebeab6df7d775610bc5
phillips <qwertysonata@gmail.com>
2024-06-16 23:23:49 +0530

    ok

--- src/pages/WeatherCard.jsx
+++ src/pages/WeatherCard.jsx
@@ -1,28 +1,28 @@
 export const WeatherCard = (props) => {
-    const { data, city } = props;
-    const { weather, main, clouds, wind } = data;
-    
-    const localTime = new Date((data.dt + city.timezone) * 1000);
-    let hours = localTime.getUTCHours();
-    const minutes = localTime.getUTCMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    if (hours === 0) {
-      hours = 12;
-    }
-    
-    return (
-      <div className="py-2 flex flex-col items-center  bg-gray-800 shadow-sm rounded-xl mx-2 w-36 flex-shrink-0">
-        <p className="text-yellow-200">{localTime.toLocaleString('default', { weekday: 'short' })}</p>
-        <p className="text-yellow-200">{`${localTime.getDate()} ${localTime.toLocaleString('default', { month: 'short' })}`}</p>
-        <p className="text-yellow-200">{`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${ampm}`}</p>
-        <br />
-        <p className="text-yellow-400">{weather[0].description}</p>
-        <p>{(main.temp - 273.15).toFixed(1)} °C</p>
-        <p>H: {main.humidity} %</p>
-        <p>{wind.speed} m/s</p>
-        <p>cloud: {clouds.all} %</p>
-      </div>
-    );
-  };
-  
+  const { data, city } = props;
+  const { weather, main, clouds, wind } = data;
+
+  // Convert the timestamp to UTC based on the city's timezone offset
+  const utcTime = new Date((data.dt + city.timezone) * 1000);
+  let utcHours = utcTime.getUTCHours();
+  const utcMinutes = utcTime.getUTCMinutes();
+  const utcAmpm = utcHours >= 12 ? 'PM' : 'AM';
+  utcHours = utcHours % 12;
+  if (utcHours === 0) {
+    utcHours = 12;
+  }
+
+  return (
+    <div className="py-2 flex flex-col items-center bg-gray-800 shadow-sm rounded-xl mx-2 w-36 flex-shrink-0">
+      <p className="text-yellow-200">{utcTime.toUTCString().split(',')[0]}</p>
+      <p className="text-yellow-200">{`${utcTime.getUTCDate()} ${utcTime.toUTCString().split(' ')[2]}`}</p>
+      <p className="text-yellow-200">{`${utcHours.toString().padStart(2, '0')}:${utcMinutes.toString().padStart(2, '0')} ${utcAmpm}`}</p>
+      <br />
+      <p className="text-yellow-400">{weather[0].description}</p>
+      <p>{(main.temp - 273.15).toFixed(1)} °C</p>
+      <p>H: {main.humidity} %</p>
+      <p>{wind.speed} m/s</p>
+      <p>Cloud: {clouds.all} %</p>
+    </div>
+  );
+};
