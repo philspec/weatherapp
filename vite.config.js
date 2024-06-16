ef48bf295591df12cedef18a55dcfcc38a8da5a1
phillips <qwertysonata@gmail.com>
2024-06-16 11:08:38 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {}, // This line is necessary for Vite to replace `process.env` references in your code
-  },
+  
   resolve: {
     alias: {
       "@": path.resolve("./src"),
